79690093394b3d7fc6e4686662ed0ed9e6bfa09c
Amine BAMBRIK <amine.bambrik.development@gmail.com>
2020-07-04 19:40:01 +0100

    Update main to take mode as command line arg

--- main.py
+++ main.py
@@ -17,11 +17,17 @@ def main():
         metavar='config_json_file',
         default='None',
         help='The Configuration file in json format')
+    arg_parser.add_argument(
+        'mode',
+        metavar='mode',
+        default='None',
+        help='Run mode train, test, or visualize')
     args = arg_parser.parse_args()
     # parse the config json file
     config = process_config(args.config)
     setup_logging(config.log_dir)
-
+    if args.mode:
+        config.mode=args.mode
     # Create the Agent and pass all the configuration to it then run it..
     agent_class = globals()[config.agent]
     agent = agent_class(config)
