cbf23fb99cb3dae4f063b372499813c654acbbc2
Amine BAMBRIK <amine.bambrik.development@gmail.com>
2020-07-05 13:06:44 +0100

    Update lenet to accept 1 channel as input

--- models/lenet5.py
+++ models/lenet5.py
@@ -5,7 +5,7 @@ from models.layers.basic_decoder import BasicDecoder
 class LeNet5(nn.Module):
   def __init__(self):
     super(LeNet5,self).__init__()
-    self.conv1 = nn.Conv2d(3,6,5)
+    self.conv1 = nn.Conv2d(1,6,5)
     self.pool = nn.MaxPool2d(2,2)
     self.conv2 = nn.Conv2d(6,16,5)
     self.decoder = BasicDecoder([16*9*9,256,512],7);
