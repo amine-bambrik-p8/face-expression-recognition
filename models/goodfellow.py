559ab950ceef60c554516cd86954b1396d8f13e7
Amine BAMBRIK <amine.bambrik.development@gmail.com>
2020-07-19 19:17:38 +0100

    Fix typo

--- models/goodfellow.py
+++ models/goodfellow.py
@@ -1,6 +1,6 @@
 import torch.nn as nn
 import torch.nn.functional as F
-from models.layers.basic_decode import BasicDecoder
+from models.layers.basic_decoder import BasicDecoder
 
 from models.layers.conv_block import conv_block
 from models.layers.stack_block import stack_block
