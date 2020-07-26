ef3e19a7afcb956fce32481667623e04e2de37e7
Amine BAMBRIK <amine.bambrik.development@gmail.com>
2020-07-26 11:51:14 +0100

    Update face utils to preload detectors once

--- utils/face_utils.py
+++ utils/face_utils.py
@@ -8,20 +8,22 @@ import cv2
 import os
 import matplotlib.pyplot as plt
 
+haarcascade = "haarcascade_frontalface_alt2.xml"
+detector = cv2.CascadeClassifier(haarcascade)
 def detect_faces(image_tensor):
-    haarcascade = "haarcascade_frontalface_alt2.xml"
-    detector = cv2.CascadeClassifier(haarcascade)
+    global detector
     image = (image_tensor.permute(1, 2, 0).numpy() * 255).astype('uint8')
     image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
     #image = cv2.imread(pic)
     faces = detector.detectMultiScale(image)
     return faces
 
+LBFmodel = "lbfmodel.yaml"
+landmark_detector  = cv2.face.createFacemarkLBF()
+landmark_detector.loadModel(LBFmodel)
 def extract_landmarks(image_tensor,faces):
+    global landmark_detector
     # Detect landmarks on "image_gray"
-    LBFmodel = "lbfmodel.yaml"
     image = (image_tensor.permute(1, 2, 0).numpy() * 255).astype('uint8')
-    landmark_detector  = cv2.face.createFacemarkLBF()
-    landmark_detector.loadModel(LBFmodel)
     _, landmarks = landmark_detector.fit(image, faces)
     return torch.tensor(landmarks)
