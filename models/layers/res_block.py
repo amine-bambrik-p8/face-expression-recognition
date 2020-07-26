9a6525761b70edfb3d8e761e40a444d46e9e48c5
Amine BAMBRIK <amine.bambrik.development@gmail.com>
2020-07-26 06:42:30 +0100

    Update resblock to remove outgate

--- models/layers/res_block.py
+++ models/layers/res_block.py
@@ -84,7 +84,7 @@ class ResNetLayer(nn.Module):
     """
     A ResNet layer composed by `n` blocks stacked one after the other
     """
-    def __init__(self, in_channels, out_channels, block=ResNetBasicBlock, n=1,out_gate=nn.Identity(), *args, **kwargs):
+    def __init__(self, in_channels, out_channels, block=ResNetBasicBlock, n=1, *args, **kwargs):
         super().__init__()
         # 'We perform downsampling directly by convolutional layers that have a stride of 2.'
         downsampling = 2 if in_channels != out_channels else 1
@@ -92,7 +92,6 @@ class ResNetLayer(nn.Module):
             block(in_channels , out_channels, *args, **kwargs, downsampling=downsampling),
             *[block(out_channels * block.expansion, 
                     out_channels, downsampling=1, *args, **kwargs) for _ in range(n - 1)],
-            out_gate
         )
 
     def forward(self, x):
