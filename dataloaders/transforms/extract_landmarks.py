730e65d6317629d49d5881419521eb7ed01d4374
Amine BAMBRIK <amine.bambrik.development@gmail.com>
2020-07-26 11:41:16 +0100

    Update extract_landmarks transform to use image size

--- dataloaders/transforms/extract_landmarks.py
+++ dataloaders/transforms/extract_landmarks.py
@@ -4,8 +4,9 @@ from utils.face_utils import extract_landmarks
 import numpy as np
 
 class ExtractLandmarks(object):
-    def __call__(self, image,width,height):
-        faces=cv2.UMat(np.array([[0,0,width,height]], dtype=np.uint8))
+    def __call__(self, image):
+        c,w,h = image.shape
+        faces=cv2.UMat(np.array([[0,0,w,h]], dtype=np.uint8))
         landmarks = extract_landmarks(image,faces)
         return image,landmarks
     def __repr__(self):
