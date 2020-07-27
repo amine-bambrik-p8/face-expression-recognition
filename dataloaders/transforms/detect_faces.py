087ca14ea7e0c1ddf30f369022f91d5f51c538f7
Amine BAMBRIK <amine.bambrik.development@gmail.com>
2020-07-27 10:45:34 +0100

    Fix image cropping

--- dataloaders/transforms/detect_faces.py
+++ dataloaders/transforms/detect_faces.py
@@ -8,8 +8,11 @@ class DetectFaces(object):
         faces = detect_faces(image)
         if len(faces) == 0:
             return F.to_grayscale(pil_image,1)
-        face = faces[0]
-        cropped_image = image[:,face[0]:face[2], face[1]:face[3]]
+        top, right, bottom, left = faces[0]
+        x0, y0 = left, bottom
+        x1, y1 = right, top
+        x2, x3 = x1-w,  x0+w
+        cropped_image = image[:,y1:y0, x2:x3]
         cropped_pil_image = F.to_pil_image(cropped_image, mode=None)
         c,w,h = image.shape
         pil_image=F.resize(cropped_pil_image,(w,h))
