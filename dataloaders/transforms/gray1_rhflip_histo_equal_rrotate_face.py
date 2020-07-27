6ddb7840f48dbfc2a4173ab2c3f0b7afc1ac3455
Amine BAMBRIK <amine.bambrik.development@gmail.com>
2020-07-27 07:57:20 +0100

    Add resize after rotation

--- dataloaders/transforms/gray1_rhflip_histo_equal_rrotate_face.py
+++ dataloaders/transforms/gray1_rhflip_histo_equal_rrotate_face.py
@@ -10,7 +10,7 @@ def transform():
                         DetectFaces(),
                         transforms.RandomRotation(30),
                         transforms.RandomHorizontalFlip(p=0.5),
-
+                        transforms.Resize(48),
                         transforms.ToTensor(),
                         transforms.Normalize([0.5],[0.5]),
                     ])
