148bcc4afb92d79247e50465e45cb07d79ffeaec
Amine BAMBRIK <amine.bambrik.development@gmail.com>
2020-07-27 07:25:33 +0100

    Update detect face transform

--- dataloaders/transforms/detect_faces.py
+++ dataloaders/transforms/detect_faces.py
@@ -2,7 +2,8 @@ import cv2
 from utils.face_utils import extract_landmarks
 import torchvision.transforms.functional as F
 class DetectFaces(object):
-    def __call__(self, image):
+    def __call__(self, pil_image):
+        image = F.to_tensor(pil_image)
         faces = detect_faces(image)
         c,w,h = image.shape
         if len(faces) == 0:
@@ -10,7 +11,7 @@ class DetectFaces(object):
         face = faces[0]
         cropped_image = image[:,face[0]:face[2], face[1]:face[3]]
         cropped_pil_image = F.to_pil_image(cropped_image, mode=None)
-        pil_image=F.resize(cropped_pil_image,(c,w,h))
-        return F.to_tensor(pil_image)
+        pil_image=F.resize(cropped_pil_image,(w,h))
+        return pil_image
     def __repr__(self):
         return self.__class__.__name__ + '()'
--- dataloaders/transforms/gray1_rhflip_histo_equal_rrotate_face.py
+++ dataloaders/transforms/gray1_rhflip_histo_equal_rrotate_face.py
@@ -6,11 +6,11 @@ def transform():
     return transforms.Compose([
                         transforms.Grayscale(1),
                         HistogramEqualization(),
+                        transforms.Grayscale(3),
+                        DetectFaces(),
                         transforms.RandomRotation(30),
                         transforms.RandomHorizontalFlip(p=0.5),
-                        transforms.RandomCrop(40),
-                        transforms.Resize(48),
+
                         transforms.ToTensor(),
                         transforms.Normalize([0.5],[0.5]),
-                        DetectFaces(),
                     ])
