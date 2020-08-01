dd27046b7d211ffa353c0a51a8367e81f78eef0c
Amine BAMBRIK <amine.bambrik.development@gmail.com>
2020-08-01 13:26:20 +0100

    Add deeper network with down sampling

---
+++ models/goodfellow_v3_down_deep.py
@@ -0,0 +1,46 @@
+import torch.nn as nn
+import torch.nn.functional as F
+from models.layers.basic_decoder import BasicDecoder
+
+from models.layers.conv_block import conv_block
+from models.layers.stack_block import stack_block
+from models.layers.same_conv import same_conv_block
+class EncoderBNDO(nn.Module):
+    def __init__(self,in_c,channels):
+        super().__init__()
+        self.enc_blocks = nn.Sequential(
+            stack_block(
+              in_f=in_c,
+              out_f=channels[0],
+              kernel_size=7,
+              block=same_conv_block,
+              depth=3,
+              out_gate=nn.Conv2d(channels[0],channels[0],kernel_size=2,stride=2),
+              conv_block=conv_block
+              ),
+            *[stack_block(
+              in_f=in_f,
+              out_f=out_f,
+              kernel_size=3,
+              block=same_conv_block,
+              depth=3,
+              out_gate=nn.Sequential(
+                  nn.Conv2d(out_f,out_f,kernel_size=2,stride=2),
+                  nn.Dropout(0.1)
+                ),
+              conv_block=conv_block
+              ) for in_f,out_f in zip(channels[:-1],channels[1:])],
+        )
+    def forward(self, x):
+        return self.enc_blocks(x)
+class GoodFellowV3DownDeep(nn.Module):
+  def __init__(self):
+    super(GoodFellowV3DownDeep,self).__init__()
+    self.encoder = EncoderBNDO(1,[64,64,128])
+    self.decoder = BasicDecoder([128*6*6,1024,1024],7,dropout=0.25)
+
+  def forward(self,x):
+    x = self.encoder(x)
+    x = x.view(x.size(0),-1)
+    x = self.decoder(x)
+    return F.log_softmax(x,dim=1)
