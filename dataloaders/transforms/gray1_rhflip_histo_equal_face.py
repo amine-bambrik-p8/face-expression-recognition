4bc1fd32f94ba309fa3be81fa81131c4656a9e1a
Amine BAMBRIK <amine.bambrik.development@gmail.com>
2020-08-05 00:47:45 +0100

    Update transform to include hflip

--- dataloaders/transforms/gray1_rhflip_histo_equal_face.py
+++ dataloaders/transforms/gray1_rhflip_histo_equal_face.py
@@ -6,6 +6,7 @@ def transform():
     return transforms.Compose([
                         transforms.Grayscale(1),
                         HistogramEqualization(),
+                        transforms.RandomHorizontalFlip(p=0.5),
                         DetectFaces(),
                         transforms.ToTensor(),
                         transforms.Normalize([0.5],[0.5]),
