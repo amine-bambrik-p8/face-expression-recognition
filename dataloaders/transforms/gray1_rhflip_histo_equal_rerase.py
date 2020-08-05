5cdfbfe9868d1a4580ea8078e46dfebff3b3ee78
Amine BAMBRIK <amine.bambrik.development@gmail.com>
2020-08-05 12:18:54 +0100

    Add rerase transform

--- dataloaders/transforms/gray1_rhflip_histo_equal_rerase.py
+++ dataloaders/transforms/gray1_rhflip_histo_equal_rerase.py
@@ -1,7 +1,10 @@
 from torchvision import transforms
+from dataloaders.transforms.histo_equal import HistogramEqualization
+
 def transform():
     return transforms.Compose([
                         transforms.Grayscale(1),
+                        HistogramEqualization(),
                         transforms.RandomHorizontalFlip(p=0.5),
                         transforms.RandomErasing(0.2,(0.2,0.2)),
                         transforms.ToTensor(),
