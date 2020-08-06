142bbe64888cb8f7583698395c9c935b5a3b0631
Amine BAMBRIK <amine.bambrik.development@gmail.com>
2020-08-06 08:37:26 +0100

    Fix rerase transform

--- dataloaders/transforms/gray1_rhflip_histo_equal_rerase.py
+++ dataloaders/transforms/gray1_rhflip_histo_equal_rerase.py
@@ -5,8 +5,8 @@ def transform():
     return transforms.Compose([
                         transforms.Grayscale(1),
                         HistogramEqualization(),
-                        transforms.RandomHorizontalFlip(p=0.5),
                         transforms.RandomErasing(0.2,(0.2,0.2)),
+                        transforms.RandomHorizontalFlip(p=0.5),
                         transforms.ToTensor(),
                         transforms.Normalize([0.5],[0.5]),
                     ])
