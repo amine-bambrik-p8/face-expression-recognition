84196789c497539de3c0058d907580e0cff69063
Amine BAMBRIK <amine.bambrik.development@gmail.com>
2020-08-07 08:11:40 +0100

    Update transform to use face detection before any transform

--- dataloaders/transforms/gray_scale_all.py
+++ dataloaders/transforms/gray_scale_all.py
@@ -6,12 +6,12 @@ from dataloaders.transforms.detect_faces import DetectFaces
 def transform():
     return transforms.Compose([
                     transforms.Grayscale(1),
+                    DetectFaces(),
                     HistogramEqualization(),
                     transforms.RandomRotation(30),
                     transforms.RandomHorizontalFlip(p=0.5),
                     transforms.CenterCrop(42),
                     transforms.Resize(48),
-                    DetectFaces(),
                     transforms.ToTensor(),
                     transforms.RandomErasing(0.5,(0.2,0.2)),
                     transforms.Normalize([0.5],[0.5]),
