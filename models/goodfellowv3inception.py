f654c1ae2c8c8062d75bb7908f7ce0b1838d5990
Amine BAMBRIK <amine.bambrik.development@gmail.com>
2020-08-10 00:28:11 +0100

    Update inception

--- models/goodfellowv3inception.py
+++ models/goodfellowv3inception.py
@@ -3,15 +3,15 @@ import torch.nn.functional as F
 from models.layers.basic_decoder import BasicDecoder
 from models.layers.inception_block import InceptionBlock
 
+from models.layers.conv_block import conv_block
+from models.layers.stack_block import stack_block
+from models.layers.same_conv import same_conv_block
 def block(in_f, out_f,dropout=0.0):
     return nn.Sequential(
-        InceptionBlock(in_f, out_f,kernel_size=3,padding=1),
+        InceptionBlock(in_f, out_f),
         nn.BatchNorm2d(out_f),
         nn.ReLU(),
-        InceptionBlock(out_f, out_f,kernel_size=3,padding=1),
-        nn.BatchNorm2d(out_f),
-        nn.ReLU(),
-        InceptionBlock(out_f, out_f,kernel_size=3,padding=1),
+        InceptionBlock(out_f, out_f),
         nn.BatchNorm2d(out_f),
         nn.ReLU(),
         nn.MaxPool2d(2,2),
@@ -20,12 +20,25 @@ def block(in_f, out_f,dropout=0.0):
 class GoodFellowV3Inception(nn.Module):
   def __init__(self,config):
     super(GoodFellowV3Inception,self).__init__()
-    self.block1 = block(1,64)
-    self.block2 = block(64,64,dropout=0.1)
-    self.block3 = block(64,128,dropout=0.1)
-    self.decoder = BasicDecoder([128*6*6,1024,1024],7,dropout=0.25)
+    self.gate = stack_block(
+              in_f=1,
+              out_f=64,
+              kernel_size=7,
+              block=same_conv_block,
+              depth=2,
+              out_gate=nn.MaxPool2d(
+                kernel_size=2,
+                stride=2
+                ),
+              conv_block=conv_block
+              )
+    self.block1 = block(64,128)
+    self.block2 = block(128,256)
+    self.block3 = block(256,512)
+    self.decoder = BasicDecoder([512*6*6,1024,1024],7)
 
   def forward(self,x):
+    x = self.gate(x)
     x = self.block1(x)
     x = self.block2(x)
     x = self.block3(x)
