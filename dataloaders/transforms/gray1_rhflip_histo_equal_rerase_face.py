27ca1467916a6d92dca85557df911b407de12909
Amine BAMBRIK <amine.bambrik.development@gmail.com>
2020-08-09 12:19:40 +0100

    Add face detection

---
+++ dataloaders/transforms/gray1_rhflip_histo_equal_rerase_face.py
@@ -0,0 +1,13 @@
+from torchvision import transforms
+from dataloaders.transforms.histo_equal import HistogramEqualization
+
+def transform():
+    return transforms.Compose([
+                        transforms.Grayscale(1),
+                        DetectFaces(),
+                        HistogramEqualization(),
+                        transforms.RandomHorizontalFlip(p=0.5),
+                        transforms.ToTensor(),
+                        transforms.RandomErasing(0.5,(0.2,0.2)),
+                        transforms.Normalize([0.5],[0.5]),
+                    ])
