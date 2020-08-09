6104b5fde581076441e415bbfcaa5fbabe731dbe
Amine BAMBRIK <amine.bambrik.development@gmail.com>
2020-08-09 16:50:50 +0100

    Filter models

--- models/goodfellowv3resnet.py
+++ models/goodfellowv3resnet.py
@@ -7,8 +7,7 @@ class GoodFellowV3ResNet(nn.Module):
     def __init__(self, in_channels=1, n_classes=7, *args, **kwargs):
         super().__init__()
         self.encoder = ResNetEncoder(in_channels, block=ResNetBasicBlock,blocks_sizes=[64, 64,128], deepths=[2, 2, 2])
-        self.decoder = BasicDecoder([128*12*12,1024,1024],7,dropout=0.25)
-        
+        self.decoder = BasicDecoder([128*12*12,1024,1024],7)
     def forward(self, x):
         x = self.encoder(x)
         x = x.view(x.size(0),-1)
--- models/layers/depthwise_separable_conv.py
+++ models/layers/depthwise_separable_conv.py
@@ -1,8 +1,8 @@
 from torch import nn
 class DSConv2d(nn.Module):
-    def __init__(self, nin, nout):
-        super(depthwise_separable_conv, self).__init__()
-        self.depthwise = nn.Conv2d(nin, nin, kernel_size=3, padding=1, groups=nin)
+    def __init__(self, nin, nout,kernel_size=3):
+        super(DSConv2d, self).__init__()
+        self.depthwise = nn.Conv2d(nin, nin, kernel_size=kernel_size, padding=kernel_size//2, groups=nin)
         self.pointwise = nn.Conv2d(nin, nout, kernel_size=1)
 
     def forward(self, x):
