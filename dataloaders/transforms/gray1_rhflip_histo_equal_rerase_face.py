e6f71eac78778c329608c0b6ae6fec5f051c5395
Amine BAMBRIK <amine.bambrik.development@gmail.com>
2020-08-09 12:29:26 +0100

    Fix import detect faces transform

--- dataloaders/transforms/gray1_rhflip_histo_equal_rerase_face.py
+++ dataloaders/transforms/gray1_rhflip_histo_equal_rerase_face.py
@@ -1,11 +1,13 @@
 from torchvision import transforms
 from dataloaders.transforms.histo_equal import HistogramEqualization
+from dataloaders.transforms.detect_faces import DetectFaces
+
 
 def transform():
     return transforms.Compose([
                         transforms.Grayscale(1),
-                        DetectFaces(),
                         HistogramEqualization(),
+                        DetectFaces(),
                         transforms.RandomHorizontalFlip(p=0.5),
                         transforms.ToTensor(),
                         transforms.RandomErasing(0.5,(0.2,0.2)),
