aa3cb6fc38075949ea5cfb8221a687b2c419e179
Amine BAMBRIK <amine.bambrik.development@gmail.com>
2020-08-10 00:58:12 +0100

    Update network

--- models/goodfellow_v3.py
+++ models/goodfellow_v3.py
@@ -2,28 +2,17 @@ import torch.nn as nn
 import torch.nn.functional as F
 from models.layers.basic_decoder import BasicDecoder
 import torch
+from models.layers import * 
 from models.layers.conv_block import conv_block
 from models.layers.stack_block import stack_block
 from models.layers.same_conv import same_conv_block
 class EncoderBNDO(nn.Module):
-    def __init__(self,in_c,channels):
+    def __init__(self,config):
         super().__init__()
         self.enc_blocks = nn.Sequential(
-            stack_block(
-              in_f=in_c,
-              out_f=channels[0],
-              kernel_size=7,
-              block=same_conv_block,
-              depth=2,
-              out_gate=nn.MaxPool2d(
-                kernel_size=2,
-                stride=2
-                ),
-              conv_block=conv_block
-              ),
             *[stack_block(
-              in_f=in_f,
-              out_f=out_f,
+              in_f=in_c,
+              out_f=out_c,
               kernel_size=5,
               block=same_conv_block,
               depth=2,
@@ -34,21 +23,36 @@ class EncoderBNDO(nn.Module):
                   ),
                 ),
               conv_block=conv_block
-              ) for in_f,out_f in zip(channels[:-1],channels[1:])],
+              ) for in_f,out_f in zip(config.encoder_channels[:-1],config_channels[1:])],
         )
     def forward(self, x):
         return self.enc_blocks(x)
 class GoodFellowV3(nn.Module):
   def __init__(self,config):
     super(GoodFellowV3,self).__init__()
-    self.encoder = EncoderBNDO(1,[64,64,128])
-    self.decoder = BasicDecoder([128*6*6,1024,1024],7)
-
+    self.gate = stack_block(
+              in_f=config.in_channels,
+              out_f=config.encoder_channels[0],
+              kernel_size=7,
+              block=same_conv_block,
+              depth=2,
+              out_gate=nn.MaxPool2d(
+                kernel_size=2,
+                stride=2
+                ),
+              conv_block=conv_block
+              )
+    self.encoder = EncoderBNDO(config)
+    #1,[64,64,128]
+    self.decoder = globals()[config.decoder](config)
+    #[128*6*6,1024,1024],7
+    self.class_fn = globals()[config.class_fn](dim=1)
   def forward(self,x):
+    x = self.gate(x)    
     x = self.encoder(x)
     x = x.view(x.size(0),-1)
     x = self.decoder(x)
-    return F.log_softmax(x,dim=1)
+    return self.classfn(x)
   
 
 class GoodFellowV3Inference(nn.Module):
@@ -56,7 +60,7 @@ class GoodFellowV3Inference(nn.Module):
     super(GoodFellowV3Inference,self).__init__()
     
     self.encoder = EncoderBNDO(1,[64,64,128])
-    self.decoder = BasicDecoder([128*7*7,1024,1024],7,dropout=0.1)
+    self.decoder = BasicDecoder([128*6*6,1024,1024],7)
 
   def forward(self,x):
     x = x.reshape(48, 48, 4,-1)
