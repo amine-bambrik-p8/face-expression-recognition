2b730fafc1d3b01ac29ea1e9adb508d4a231fc1e
Amine BAMBRIK <amine.bambrik.development@gmail.com>
2020-08-10 02:54:45 +0100

    Update data aug configs

--- models/layers/avg_decoder.py
+++ models/layers/avg_decoder.py
@@ -7,10 +7,12 @@ class AvgDecoder(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.avg = nn.AdaptiveAvgPool2d((1,1))
+        self.dropout = nn.Dropout(config.dropout) if config.dropout>0.0 else nn.Identity()
         self.decoder = nn.Linear(config.decoder_channels[0],config.n_classes)
 
     def forward(self, x):
         x = self.avg(x)
         x = x.view(x.size(0), -1)
+        x = self.dropout(x)
         x = self.decoder(x)
         return x
