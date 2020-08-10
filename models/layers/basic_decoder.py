fcefa89074cd63cc357bec89220fa6291a26dd76
Amine BAMBRIK <amine.bambrik.development@gmail.com>
2020-08-10 21:37:01 +0100

    Update decoder to have activation fucntion as param

--- models/layers/basic_decoder.py
+++ models/layers/basic_decoder.py
@@ -1,11 +1,11 @@
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn import *
-def dec_block(in_f, out_f,dropout=0.0,activation=nn.ReLU,batch_norm=True):
+def dec_block(in_f, out_f,dropout=0.0,activation=nn.ReLU(),batch_norm=True):
     return nn.Sequential(
         nn.Linear(in_f, out_f),        
         nn.BatchNorm1d(out_f) if(batch_norm) else nn.Identity(),
-        activation(),
+        activation,
         nn.Dropout(dropout) if(dropout > 0.0) else nn.Identity()
     )
 class BasicDecoder(nn.Module):
@@ -15,7 +15,7 @@ class BasicDecoder(nn.Module):
         n_classes=config.n_classes
         dropout=config.decoder_dropout
         batch_norm=config.decoder_batch_norm
-        self.dec_blocks = nn.Sequential(*[dec_block(in_f, out_f,dropout=dropout,batch_norm=batch_norm,activation=globals()[config.decoder_fn]) 
+        self.dec_blocks = nn.Sequential(*[dec_block(in_f, out_f,dropout=dropout,batch_norm=batch_norm,activation=globals()[config.decoder_fn_params](*config.decoder_fn_params)) 
                     for in_f, out_f in zip(dec_sizes, dec_sizes[1:])])
         self.last = nn.Linear(dec_sizes[-1], n_classes)
     def forward(self, x):
