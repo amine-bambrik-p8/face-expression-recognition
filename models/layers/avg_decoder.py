12c6552dba91f2cc59bd43ed928a829d0210e179
Amine BAMBRIK <amine.bambrik.development@gmail.com>
2020-08-11 01:06:48 +0100

    Fix config param name

--- models/layers/avg_decoder.py
+++ models/layers/avg_decoder.py
@@ -7,7 +7,7 @@ class AvgDecoder(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.avg = nn.AdaptiveAvgPool2d((1,1))
-        self.dropout = nn.Dropout(config.decoder_dropout) if config.dropout>0.0 else nn.Identity()
+        self.dropout = nn.Dropout(config.decoder_dropout) if config.decoder_dropout>0.0 else nn.Identity()
         self.decoder = nn.Linear(config.decoder_channels[0],config.n_classes)
 
     def forward(self, x):
