8c35b985f45e0925a3ea40af44ea001ce593cdf7
Amine BAMBRIK <amine.bambrik.development@gmail.com>
2020-08-11 14:15:43 +0100

    Update imports

--- models/goodfellowv3resnet.py
+++ models/goodfellowv3resnet.py
@@ -2,6 +2,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 from models.layers.res_block import *
 from models.layers.basic_decoder import BasicDecoder
+from models.layers.avg_decoder import AvgDecoder
+from models.layers.net_in_net_decoder import NetInNetDecoder
 from torch.nn import *
 class GoodFellowV3ResNet(nn.Module):
     def __init__(self,config, in_channels=1, n_classes=7, *args, **kwargs):
