10d973cf27977d987a6ab75210bc75f6156fdab3
Amine BAMBRIK <amine.bambrik.development@gmail.com>
2020-08-12 13:33:58 +0100

    Update net in net to have voting

--- models/layers/net_in_net_decoder.py
+++ models/layers/net_in_net_decoder.py
@@ -2,9 +2,16 @@
 import torch
 from torch import nn
 from torch.nn import *
-from models.layers.conv_block import conv_block
 from models.layers.stack_block import stack_block
 from models.layers.same_conv import same_conv_block
+def conv_block(in_f, out_f,kernel_size,batch_norm=True,dropout=0.0,activation=nn.ReLU(),*args, **kwargs):
+    return nn.Sequential(
+        nn.Conv2d(in_f, out_f,kernel_size=kernel_size, *args, **kwargs),
+        nn.BatchNorm2d(out_f) if(batch_norm) else nn.Identity(),
+        nn.LocalResponseNorm(out_f) if(batch_norm) else nn.Identity(),
+        activation,
+        nn.Dropout2d(dropout) if(dropout > 0.0) else nn.Identity()
+    )
 class NetInNetDecoder(nn.Module):
     """
     This class represents the tail of ResNet. It performs a global pooling and maps the output to the
@@ -23,12 +30,15 @@ class NetInNetDecoder(nn.Module):
               dropout=config.decoder_dropout,
               batch_norm=config.decoder_batch_norm,
               ) for in_c,out_c in zip(config.decoder_channels[:-1],config.decoder_channels[1:]) ])
+        self.decoder = nn.Sequential(
+            nn.Conv2d(config.decoder_channels[-1],config.n_classes,kernel_size=1),
+            nn.Softmax2d()
+            )
         self.avg = nn.AdaptiveAvgPool2d((1,1))
-        self.decoder = nn.Linear(config.decoder_channels[-1],config.n_classes)
 
     def forward(self, x):
         x = self.net_in_net(x)
+        x = self.decoder(x)
         x = self.avg(x)
         x = torch.flatten(x,1)
-        x = self.decoder(x)
         return x
