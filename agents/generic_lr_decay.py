352e54428c68647e2395e92e59e92c8df3fc9cc3
Amine BAMBRIK <amine.bambrik.development@gmail.com>
2020-08-13 01:55:59 +0100

    Update learning rate decay

--- agents/generic_lr_decay.py
+++ agents/generic_lr_decay.py
@@ -9,7 +9,7 @@ class GenericAgentLRDecay(GenericAgent):
         super().__init__(config)
 
     def train(self):
-        scheduler = optim.lr_scheduler.ReduceLROnPlateau(self.optimizer,factor=self.config.lr_scheduler_factor,mode=self.config.lr_scheduler_mode,patience=2,min_lr=self.config.min_lr)
+        scheduler = optim.lr_scheduler.ReduceLROnPlateau(self.optimizer,factor=self.config.lr_scheduler_factor,mode=self.config.lr_scheduler_mode,patience=4,min_lr=self.config.min_lr)
         for epoch in range(1, self.config.max_epoch + 1):
             (train_loss,train_accuracy) = self.train_one_epoch()
             if epoch % self.config.validate_every == self.config.validate_every-1:
