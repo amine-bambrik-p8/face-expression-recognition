d323e4da1c855ed5d7ddb35422a2180bb003a51f
Amine BAMBRIK <amine.bambrik.development@gmail.com>
2020-08-13 15:02:28 +0100

    Update res block to hqve gqte

--- models/layers/res_block.py
+++ models/layers/res_block.py
@@ -107,18 +107,24 @@ class ResNetEncoder(nn.Module):
                  activation='relu', block=ResNetBasicBlock, *args, **kwargs):
         super().__init__()
         self.blocks_sizes = blocks_sizes
-        
-        self.in_out_block_sizes = list(zip(blocks_sizes[:-1], blocks_sizes[1:]))
+        self.gate = nn.Sequential(
+            nn.Conv2d(in_channels, self.blocks_sizes[0], kernel_size=7, stride=2, padding=3, bias=False),
+            nn.BatchNorm2d(self.blocks_sizes[0]),
+            activation_func(activation),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        )
+        self.in_out_block_sizes = list(zip(blocks_sizes, blocks_sizes[1:]))
         self.blocks = nn.ModuleList([ 
-            ResNetLayer(in_channels, blocks_sizes[0], n=deepths[0], activation=activation, 
+            ResNetLayer(blocks_sizes[0], blocks_sizes[0], n=deepths[0], activation=activation,
                         block=block,*args, **kwargs),
-            *[ResNetLayer(in_channels, 
+            *[ResNetLayer(in_channels,
                           out_channels, n=n, activation=activation, 
                           block=block, *args, **kwargs) 
               for (in_channels, out_channels), n in zip(self.in_out_block_sizes, deepths[1:])]       
         ])
                 
     def forward(self, x):
+        x = self.gate(x)
         for block in self.blocks:
             x = block(x)
         return x
