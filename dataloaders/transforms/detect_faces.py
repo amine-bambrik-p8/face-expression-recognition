e0c2fbf4ce53a365643a6336b136473a305cc607
Amine BAMBRIK <amine.bambrik.development@gmail.com>
2020-08-16 23:06:31 +0100

    Update detect face add debug

--- dataloaders/transforms/detect_faces.py
+++ dataloaders/transforms/detect_faces.py
@@ -3,12 +3,12 @@ from utils.face_utils import detect_faces
 import torchvision.transforms.functional as F
 class DetectFaces(object):
     def __call__(self, pil_image):
+        print("Hello")
         pil_image = F.to_grayscale(pil_image,3) 
         image = F.to_tensor(pil_image)
         x0,y0,x1,y1 = detect_faces(image)
-        cropped_image = image[:,x0:x1,y0:y1]
-        print("Hello")
         print(x0,y0,x1,y1)
+        cropped_image = image[:,x0:x1,y0:y1]
         cropped_pil_image = F.to_pil_image(cropped_image, mode=None)
         c,w,h = image.shape
         pil_image=F.resize(cropped_pil_image,(w,h),interpolation=PIL.Image.BICUBIC)
