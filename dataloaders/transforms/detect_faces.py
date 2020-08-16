634acc6a86cb976b3e8254b73194db63bfb5983f
Amine BAMBRIK <amine.bambrik.development@gmail.com>
2020-08-16 22:59:20 +0100

    Update face detectaction

--- dataloaders/transforms/detect_faces.py
+++ dataloaders/transforms/detect_faces.py
@@ -6,7 +6,7 @@ class DetectFaces(object):
         pil_image = F.to_grayscale(pil_image,3) 
         image = F.to_tensor(pil_image)
         x0,y0,x1,y1 = detect_faces(image)
-        cropped_image = image[:,y0:y1, x0:x1]
+        cropped_image = image[:,x0:x1,y0:y1]
         cropped_pil_image = F.to_pil_image(cropped_image, mode=None)
         c,w,h = image.shape
         pil_image=F.resize(cropped_pil_image,(w,h),interpolation=PIL.Image.BICUBIC)
