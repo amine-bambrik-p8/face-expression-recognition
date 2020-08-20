eda62ef228ff525861b8699d21a95f2821d0ac42
Amine BAMBRIK <amine.bambrik.development@gmail.com>
2020-08-20 20:17:29 +0100

    Update configs

--- agents/generic_lr_decay.py
+++ agents/generic_lr_decay.py
@@ -9,12 +9,12 @@ class GenericAgentLRDecay(GenericAgent):
         super().__init__(config)
 
     def train(self):
-        scheduler = optim.lr_scheduler.ReduceLROnPlateau(self.optimizer,factor=self.config.lr_scheduler_factor,mode=self.config.lr_scheduler_mode,patience=4,min_lr=self.config.min_lr)
+        scheduler = optim.lr_scheduler.ReduceLROnPlateau(self.optimizer,factor=self.config.lr_scheduler_factor,mode=self.config.lr_scheduler_mode,patience=10,min_lr=self.config.min_lr)
         for epoch in range(1, self.config.max_epoch + 1):
             (train_loss,train_accuracy) = self.train_one_epoch()
             if epoch % self.config.validate_every == self.config.validate_every-1:
                 (loss,accuracy) = self.validate()
-                scheduler.step(loss)
+                scheduler.step(accuracy)
                 self.summary_writer.add_scalars('accuracy', {
                         'training_{}'.format(self.config.label):train_accuracy,
                         'validation_{}'.format(self.config.label):accuracy
