1b49cef1865844a54b1c702ea062706ec37c88a9
Amine BAMBRIK <amine.bambrik.development@gmail.com>
2020-08-24 08:35:45 +0100

    Update vgg

--- models/layers/stack_block.py
+++ models/layers/stack_block.py
@@ -1,5 +1,5 @@
 import torch.nn as nn
-def stack_block(in_f, out_f,kernel_size,block,depth=1,out_gate=nn.Identity(),*args, **kwargs):
+def stack_block(in_f, out_f,kernel_size,block,depth=1,out_gate=nn.Identity(),in_gate=nn.Identity(),*args, **kwargs):
     return nn.Sequential(
         block(in_f,out_f,kernel_size,*args, **kwargs),
         *[block(out_f,out_f,kernel_size,*args, **kwargs) for i in range(1,depth)],
