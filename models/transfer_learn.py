3c5c92027d1c96957a753798d532c9412e999349
Amine BAMBRIK <amine.bambrik.development@gmail.com>
2020-08-28 14:42:04 +0100

    Add transfer learning model

---
+++ models/transfer_learn.py
@@ -0,0 +1,19 @@
+
+import torch.nn as nn
+import torch.nn.functional as F
+from models.layers.basic_decoder import BasicDecoder
+import torchvision
+class ResNet18(nn.Module):
+    def __init__(self,config):
+        super(ResNet18,self).__init__()
+        self.model_conv = torchvision.models.resnet18(pretrained=True)
+        for param in model_conv.parameters():
+            param.requires_grad = False
+        num_ftrs = self.model_conv.fc.in_features
+        config.encoder_channels=[num_ftrs]
+        self.model_conv.fc = globals()[config.decoder](config)
+    def forward(self,x):
+        x = self.model_conv(x)
+        return F.log_softmax(x,dim=1)
+    def parameters():
+        return self.model_conv.fc.parameters()
