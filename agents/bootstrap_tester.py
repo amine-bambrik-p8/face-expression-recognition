870915bd384fad3b12152d447827e52323098f97
Amine BAMBRIK <amine.bambrik.development@gmail.com>
2020-09-04 18:01:36 +0100

    Update agents

--- agents/bootstrap_tester.py
+++ agents/bootstrap_tester.py
@@ -34,7 +34,7 @@ class BootstrapTester(BaseAgent):
         self.agents = []
         self.load_checkpoint()
         self.data_loader= self.agents[0].data_loader
-        self.summary_writer.add_figure("Confusion matrix for {}".format(self.config.label),fig)
+        self.summary_writer = SummaryWriter(log_dir="./experiments/{}/summaries".format(self.config.exp_name), comment=self.config.model)
 
 
     def load_checkpoint(self):
