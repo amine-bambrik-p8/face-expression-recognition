37ad435af243d879b7920d7ec793df307f7cb29d
Amine BAMBRIK <amine.bambrik.development@gmail.com>
2020-09-06 12:39:23 +0100

    Update ckplus dataloaders

---
+++ dataloaders/ckplus_test.py
@@ -0,0 +1,65 @@
+"""
+Mnist Data loader, as given in Mnist tutorial
+"""
+import imageio
+import torch
+import torchvision
+import torchvision.utils as v_utils
+from torchvision import datasets, transforms
+import numpy as np
+from torch.utils.data import DataLoader, TensorDataset, Dataset
+from torch.utils.data.sampler import SubsetRandomSampler
+from dataloaders.transforms import *
+from utils.project_data import project_data
+class ImageCKDataLoader:
+    def __init__(self, config):
+        """
+        :param config:
+        """
+        self.config = config
+        test_transform_m = globals()[config.test_transform]
+        train_transform_m = globals()[config.train_transform]
+        valid_transform_m = globals()[config.valid_transform]
+        train_transform = getattr(train_transform_m,"transform")()
+        valid_transform = getattr(valid_transform_m,"transform")()
+        test_transform = getattr(test_transform_m,"transform")()
+        train_dataset = torchvision.datasets.ImageFolder(
+            root=self.config.train_datafolder,
+            transform=train_transform
+        )
+        valid_dataset = torchvision.datasets.ImageFolder(
+            root=self.config.valid_datafolder,
+            transform=valid_transform
+        )
+        test_dataset = torchvision.datasets.ImageFolder(
+            root=self.config.test_datafolder,
+            transform=test_transform,
+            )
+        self.valid_size = len(valid_dataset)
+        self.train_size = len(train_dataset)
+        self.test_size = len(test_dataset)
+        self.train_loader = torch.utils.data.DataLoader(train_dataset,batch_size=self.config.batch_size,num_workers=self.config.data_loader_workers,pin_memory=config.pin_memory,shuffle=True)
+        self.test_loader = torch.utils.data.DataLoader(test_dataset,batch_size=self.config.batch_size,num_workers=self.config.data_loader_workers,pin_memory=config.pin_memory)
+        self.valid_loader = torch.utils.data.DataLoader(valid_dataset,batch_size=self.config.batch_size,num_workers=self.config.data_loader_workers,pin_memory=config.pin_memory)
+        self.classes = ("anger","disgust","fear","happy","contempt","sadness","surprise")
+    def visualize_images(self,writer):
+        train_dataiter = iter(self.train_loader)
+        test_dataiter = iter(self.test_loader)
+        images, labels = train_dataiter.next()
+
+        # create grid of images
+        train_img_grid = torchvision.utils.make_grid(images)
+        
+        images, labels = test_dataiter.next()
+        test_img_grid = torchvision.utils.make_grid(images)
+        
+        # write to tensorboard
+        writer.add_image('%s Train Data: %s transfrom'%(self.config.exp_name,self.config.train_transform), train_img_grid)
+        writer.add_image('%s Test Data: %s transfrom'%(self.config.exp_name,self.config.test_transform), test_img_grid)
+    def project_data(self,writer):
+        project_data(writer,self.train_loader,self.classes)
+    def visualize(self,writer):
+        self.visualize_images(writer)
+        self.project_data(writer)
+    def finalize(self):
+        pass
