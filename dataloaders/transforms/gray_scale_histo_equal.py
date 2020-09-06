d872dbfe432beb6bffd0dd24e52194e2baca4e88
Amine BAMBRIK <amine.bambrik.development@gmail.com>
2020-09-06 14:02:04 +0100

    Update transforms

---
+++ dataloaders/transforms/gray_scale_histo_equal.py
@@ -0,0 +1,22 @@
+from torchvision import transforms
+from dataloaders.transforms.histo_equal import HistogramEqualization
+from dataloaders.transforms.detect_faces import DetectFaces
+import imgaug.augmenters as iaa
+import numpy as np
+import torchvision.transforms.functional as F
+
+class ImgAugTransform:
+    def __init__(self):
+        self.aug = iaa.Sequential([
+            iaa.HistogramEqualization(),
+        ])
+    def __call__(self, img):
+        img = np.array(img)
+        return F.to_pil_image(F.to_tensor(self.aug.augment_image(img)))
+def transform():
+    return transforms.Compose([
+                        transforms.Grayscale(1),
+                        ImgAugTransform(),
+                        transforms.ToTensor(),
+                        transforms.Normalize([0.5],[0.5]),
+                    ])
